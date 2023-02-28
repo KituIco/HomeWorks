eb71df521bc40d8813aef8f07e625d30503b7d14
KimTuico <kvtuico@up.edu.ph>
2023-02-28 12:26:17 +0800

    file structure

--- ClientProvider/App.js
+++ ClientProvider/App.js
@@ -1,19 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import AppLoading from 'expo-app-loading';
+import * as Font from "expo-font";
+
+import Navigator from './routes/mainStack';
+
+const getFonts = () => Font.loadAsync({
+  'lexend': require('./assets/fonts/Lexend-Regular.ttf'),
+  'lexend-light': require('./assets/fonts/Lexend-Light.ttf'),
+  'montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
+  'quicksand': require('./assets/fonts/Quicksand-Regular.ttf'),
+  'quicksand-light': require('./assets/fonts/Quicksand-Light.ttf'),
+  'quicksand-bold': require('./assets/fonts/Quicksand-Bold.ttf'),
+  'quicksand-medium': require('./assets/fonts/Quicksand-Medium.ttf'),
+  'notosans': require('./assets/fonts/NotoSans-Regular.ttf'),
+  'notosans-light': require('./assets/fonts/NotoSans-Light.ttf'),
+  'notosans-medium': require('./assets/fonts/NotoSans-Medium.ttf'),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Navigator />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => {}} 
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
