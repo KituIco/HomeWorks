14c4c7622d01f7545268e8a9c613e4e41c9ace69
KituIco <kimtuico_06@rocketmail.com>
2023-03-06 09:25:17 +0800

    Revert "Added file streaming of data in upload image services of client side applications."
    This reverts commit 5ee8872bc1cc5460e628fa108c2dabee93a2131d.

--- ClientProvider/services/image/image-services.js
+++ ClientProvider/services/image/image-services.js
@@ -1,12 +1,11 @@
 import { postAxios } from '../../lib/axios';
-import * as fs from 'react-native-fs'
 
 let url = '/upload';
 let ImageService = {
     uploadFile: async (data) => {
         try {
             var bodyFormData = new FormData();
-            bodyFormData.append('image', fs.createReadStream(data));
+            bodyFormData.append('image', data);
             let res = await postAxios(url, bodyFormData, true);
             return res.url;
         } catch (error) {
--- ClientSeeker/services/image/image-services.js
+++ ClientSeeker/services/image/image-services.js
@@ -1,12 +1,11 @@
 import { postAxios } from '../../lib/axios';
-import * as fs from 'react-native-fs'
 
 let url = '/upload';
 let ImageService = {
     uploadFile: async (data) => {
         try {
             var bodyFormData = new FormData();
-            /*let file = data.split("/");
+            let file = data.split("/");
             let name = file[file.length-1];
             let type = `image/${name.split(".")[1]}`;
 
@@ -14,9 +13,7 @@ let ImageService = {
                 uri: data,
                 name: name,
                 type: type,
-            });*/
-
-            bodyFormData.append('image', fs.createReadStream(data));
+            });
             let res = await postAxios(url, bodyFormData, true);
             return res.url;
         } catch (error) {
