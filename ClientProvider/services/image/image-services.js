5ee8872bc1cc5460e628fa108c2dabee93a2131d
Syyan7979 <kiisrael@up.edu.ph>
2023-03-06 09:13:42 +0800

    Added file streaming of data in upload image services of client side applications.

--- ClientProvider/services/image/image-services.js
+++ ClientProvider/services/image/image-services.js
@@ -1,11 +1,12 @@
 import { postAxios } from '../../lib/axios';
+import * as fs from 'react-native-fs'
 
 let url = '/upload';
 let ImageService = {
     uploadFile: async (data) => {
         try {
             var bodyFormData = new FormData();
-            bodyFormData.append('image', data);
+            bodyFormData.append('image', fs.createReadStream(data));
             let res = await postAxios(url, bodyFormData, true);
             return res.url;
         } catch (error) {
