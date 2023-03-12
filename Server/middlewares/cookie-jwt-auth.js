3d60ce1e1bbc231b487e7d98044c3f2d615a76da
Syyan7979 <kiisrael@up.edu.ph>
2023-03-12 16:47:46 +0800

    changed cookie auth to callback function.

--- Server/middlewares/cookie-jwt-auth.js
+++ Server/middlewares/cookie-jwt-auth.js
@@ -3,44 +3,28 @@ const jwt = require('jsonwebtoken');
 cookieJwtAuth = (req, res, next) => {
     let {access_token, refresh_token} = req.cookies;
         
-    jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET).then(
-        (user) => {
-            req.user = user;
-            next();
-        }
-    ).catch((error) => {
-        if (error.name === 'TokenExpiredError') {
-            jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET).then(
-                (user) => {
-                    let token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                        expiresIn: '1m'
-                    });
-                    res.cookie('access_token', token, {
-                        maxAge: 900000,
+    jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err && err.type == 'TokenExpiredError') {
+            jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    res.status(401).send('Unauthorized');
+                } else {
+                    let accessToken = jwt.sign(decoded, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1m'});
+                    res.cookie('access_token', accessToken, {
+                        maxAge: 900000
                         // httpOnly: true,
                         // secure: true
                         // domain: 'correct domain'
                     });
-                    req.user = user;
+                    req.user = decoded;
                     next();
                 }
-            ).catch(
-                (error) => {
-                    res.clearCookie("access_token");
-                    res.clearCookie("refresh_token");
-                    res.status(403).json({
-                        message: "Forbidden"
-                    });
-                }
-            )
-        } else {
-            res.clearCookie("access_token");
-            res.clearCookie("refresh_token");
-            res.status(403).json({
-                message: "Forbidden"
             });
         }
+        req.user = decoded;
+        next();
     });
+        
 }
 
 module.exports = cookieJwtAuth;
